c29130bf9334bf79a088dd23dc5b741a4ae90fab
Nikola <jonnywintech@gmail.com>
2025-12-10 21:26:40 +0100

    adding initial router setup

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:fitness/app/app_router.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final GoRouter router = buildRouter();
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
+  }
+}
--- lib/app/design/design_tokens.dart
+++ lib/app/design/design_tokens.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/cupertino.dart';
 
+class DT {
+  static const s1 = 4.0,
+      s2 = 8.0,
+      s3 = 12.0,
+      s4 = 16.0,
+      s5 = 20,
+      s6 = 24.0,
+      s8 = 32.0;
+
+  static const bg = Color(0xFF2EEF9);
+  static const text = Color(0xFF0FF1115);
+  static const cardYellow = Color(0xFFFFC85D);
+  static const cardBlue = Color(0xFFbbD2FF);
+  static const cardGreen = Color(0xFFE7F8ED);
+  static const cardOrange = Color(0xFFFFEED6);
+}
